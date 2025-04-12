654cd3ea61c8968fdfeec088814bbabb96a1455d
Kate <43656898+kduncanwelke@users.noreply.github.com>
2025-04-12 15:02:45 -0500

    Initial Commit

---
+++ Fetch Project/Fetch_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_ProjectApp.swift
+//  Fetch Project
+//
+//  Created by Katherine Duncan-Welke on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
